a30271fe6ffe4a6abb5ce73cd85fb2b8101b3d27
Touch Tone <touch_tone@mail.com>
2014-04-08 00:57:43 -0500

    Minor fixes

--- test_download.py
+++ test_download.py
@@ -18,7 +18,12 @@ js = jsit.JSIT(sys.argv[1], sys.argv[2])
 ar = aria.Aria(cleanupLeftovers = True)
 
 t = js.lookupTorrent("78d03cdec2deae4070631ea49944709299449e9f")
+print "Lookup:", t
 
+if not t:
+    t = js.addTorrentURL("magnet:?xt=urn:btih:06591706c10e8aa7371b521db0b594f671a614d2&dn=ShutterStock+7163029+-+Wild+Orchids+and+Pink+Swirls+%28Vector%29&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337",
+                       maximum_ratio = 0.01)        
+	
 print t.name
 
 print "Waiting for torrent to finish..."
@@ -33,15 +38,14 @@ durls = [f.url for f in t.files]
     
 print "Got %d files, downloading them." % (len(durls))
 
-d = ar.download([durls[5]],  basedir = "downloads/" + t.name, fullsize = t.size, startPaused = False)    
+d = ar.download(durls,  basedir = "downloads/" + t.name, fullsize = t.size, startPaused = False, torrentdata = t.torrent)    
 
 # Wait for aria to finish
 while not d.hasFinished:
-    sys.stdout.write("Download {percentage:.02f}% done ({numActive} running at {downloadSpeed:.0f} b/s, {numWaiting} waiting, {numStopped} stopped)".format(percentage = d.percentage, **ar.status)
-                        + " " * 10 + "\r");
+    sys.stdout.write("Download {percentage:.02f}% done ({numActive} running at {downloadSpeed:.0f} b/s, {numWaiting} waiting, {numStopped} stopped)\n".format(percentage = d.percentage, **ar.status));
     sys.stdout.flush()
-    time.sleep(1)
+    time.sleep(5)
 
 print
-print "Done. Check out 'downloads/FunnyWallpapers' for the results."
+print "Done. Check out 'downloads/' for the results."
     
