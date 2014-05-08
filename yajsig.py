0f8cecdc46bf768ecdf259e7a0702b64cf631163
TouchTone <touch_tone@mail.com>
2014-05-08 02:06:12 -0500

    Added splash screen, exception catcher, better login handling

--- yajsig.py
+++ yajsig.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
-import operator, copy, os, sys, threading
+import operator, copy, os, sys, threading, cStringIO, traceback
 from PySide.QtCore import *
 from PySide.QtGui import *
 
 import preferences
 pref = preferences.pref
-prefOrVal = preferences.prefOrVal
 
 
 from log import *
@@ -20,7 +19,6 @@ import jsit
 
 
 qApp = None
-
 
 class JSITWindow(QMainWindow):
     def __init__(self, mgr, *args):
@@ -52,21 +50,10 @@ class JSITWindow(QMainWindow):
         
         
         # Set up values from preferences
-        self.ui.watchClipboard.setChecked(pref("jsit_manager", "watchClipboard"))
-        self.ui.watchDirectory.setChecked(pref("jsit_manager", "watchDirectory"))
+        self.ui.watchClipboard.setChecked(bool(pref("jsit_manager", "watchClipboard", False)))
+        self.ui.watchDirectory.setChecked(bool(pref("jsit_manager", "watchDirectory", False)))
         
-  
-        # Put up a Message box for the first (slow) update
-        sb = QMessageBox()
-        sb.setText("Getting initial JSIT state, please wait...")
-        sb.setWindowTitle("Startup")
-        self.ui.centralwidget.show()
-        self.ui.centralwidget.adjustSize()
-        self.ui.tableView.centerDialog(sb)
-        sb.show()
-        self._startBox = sb
-        QTimer.singleShot(5000, self.closeStartBox)
-        
+                
     
     def __repr__(self):
         return "JSITWindow(0x%x)" % id(self)
@@ -85,14 +72,11 @@ class JSITWindow(QMainWindow):
             sel  = unicode_cleanup(self._clip.text(QClipboard.Selection)).encode("ascii", 'replace')
             self.model.update(clip = [clip, sel])
             
-            if pref("GUI", "threadedUpdates"):
-                updateEvent.set()
-            else:
-                self.model.updateAttributes(self.ui.tableView)
+            # self.model.updateAttributes(self.ui.tableView) Still needed? Shouldn't...
  
         finally:
             if self._visible:
-                QTimer.singleShot(pref("main", "updateRate"), self.update)
+                QTimer.singleShot(pref("main", "updateRate", 1000), self.update)
 
             
     def addTorrentFiles(self):
@@ -100,11 +84,11 @@ class JSITWindow(QMainWindow):
         fns,ftype = QFileDialog.getOpenFileNames(self, "Open Torrent File", "", "Torrent Files (*.torrent)")
         
         for fn in fns:
-            tor = self.mgr.addTorrentFile(fn, basedir=pref("downloads", "basedir"), maximum_ratio = pref("jsit", "maximumRatioPublic"), 
-                                            unquoteNames=pref("downloads", "unquoteNames"), interpretDirectories=pref("downloads", "interpretDirectories"))
+            tor = self.mgr.addTorrentFile(fn, basedir=pref("downloads", "basedir", "downloads"), maximum_ratio = pref("jsit", "maximumRatioPublic", 1.5), 
+                                            unquoteNames=pref("downloads", "unquoteNames", True), interpretDirectories=pref("downloads", "interpretDirectories", True))
                                         
             if tor.private:
-                tor.maximum_ratio = pref("jsit", "maximumRatioPrivate")
+                tor.maximum_ratio = pref("jsit", "maximumRatioPrivate", 5.0)
             
     def addTorrentURL(self):
         log(INFO)
@@ -118,11 +102,11 @@ class JSITWindow(QMainWindow):
         url = dlg.textValue()  
         
         if ok:
-            tor = self.mgr.addTorrentURL(url, basedir=pref("downloads", "basedir"), maximum_ratio=pref("jsit", "maximumRatio"), 
-                                            unquoteNames=pref("downloads", "unquoteNames"), interpretDirectories=pref("downloads", "interpretDirectories"))       
+            tor = self.mgr.addTorrentURL(url, basedir=pref("downloads", "basedir", "downloads"), maximum_ratio=pref("jsit", "maximumRatio", 1.5), 
+                                            unquoteNames=pref("downloads", "unquoteNames", True), interpretDirectories=pref("downloads", "interpretDirectories", True))       
                                         
             if tor.private:
-                tor.maximum_ratio = pref("jsit", "maximumRatioPrivate")
+                tor.maximum_ratio = pref("jsit", "maximumRatioPrivate", 5.0)
         
     def startAll(self):
         log(INFO)
@@ -173,7 +157,7 @@ class JSITWindow(QMainWindow):
             b.setDefaultButton(QMessageBox.Save);
             ret = b.exec_();
             
-            log(DEBUG, "ret= %s\n"% ret)
+            log(DEBUG, "ret= %s"% ret)
         
             if ret == QMessageBox.Save:       
                 event.handled()
@@ -186,14 +170,8 @@ class JSITWindow(QMainWindow):
                 log(WARNING, "Messagebox returned %s, not sure what to do." % ret)
         else:
             self.savePreferences()
-        
-        if pref("GUI", "threadedUpdates"):
-            log(WARNING, "Waiting for update thread to quit...\n")
-            quitEvent.set()
-            updateEvent.set()
-            updateThread.join()
 
-        log(WARNING, "Quitting.\n")
+        log(WARNING, "Quitting.")
         
         self.mgr.release()
         
@@ -202,7 +180,7 @@ class JSITWindow(QMainWindow):
     
     def savePreferences(self):
         log(INFO)
-        preferences.save("preferences.json")
+        preferences.save(os.path.join(basedir, "preferences.json"))
         
     
     def NIY(self):
@@ -215,108 +193,150 @@ class JSITWindow(QMainWindow):
         ret = b.exec_();
 
 
-# helper method to do attribute updates in separate thread
-def updateThreadFunc(model, view):
-    log(DEBUG, "Starting.\n")    
-    
-    while not quitEvent.is_set():
-    
-        log(DEBUG, "Wait\n")
-        updateEvent.wait()
-        
-        log(DEBUG, "Update\n")
-        model.updateAttributes(view)
-        
-        updateEvent.clear()
-    
-    log(DEBUG, "Ending.\n")    
+# Exception catchall helper
+# Based on http://www.riverbankcomputing.com/pipermail/pyqt/2009-May/022961.html (originally from Eric IDE)
 
-    
+exception_active = False
 
+def excepthook(excType, excValue, tracebackobj):
+    """
+    Global function to catch unhandled exceptions.
+    
+    @param excType exception type
+    @param excValue exception value
+    @param tracebackobj traceback object
+    """
+    # Not reentrant, ignore everything after the first
+    global exception_active
+    if exception_active:
+        return
+        
+    separator = '-' * 80
+    logFile = "simple.log"
+    notice = \
+        """An unhandled exception occurred. Please report the problem\n"""\
+        """at https://github.com/TouchTone/pyjsit/issues .\n"""\
+        """Please include the 'yajsig.log' and 'aria.log' log files in your report.\n\nError information:\n"""
+    
+    versionInfo = "0.3.0"
+    timeString = time.strftime("%Y-%m-%d, %H:%M:%S")    
+    
+    tbinfofile = cStringIO.StringIO()
+    traceback.print_tb(tracebackobj, None, tbinfofile)
+    tbinfofile.seek(0)
+    tbinfo = tbinfofile.read()
+    errmsg = '%s: \n%s' % (str(excType), str(excValue))
+    sections = [separator, timeString, separator, errmsg, separator, tbinfo]
+    msg = '\n'.join(sections)
+    
+    log(ERROR, "Caught unhandled exception %s!" % errmsg)
+    log(ERROR, tbinfo)
+
+    exception_active = True
+    
+    ret = QMessageBox.question(None, "Unhandled Exception Caught!", str(notice)+str(msg)+str(versionInfo), QMessageBox.Ok | QMessageBox.Cancel )
+    
+    if ret == QMessageBox.Cancel:
+        os._exit(1) # Brute force exit, avoid thread problems...
+        
+    exception_active = False
+  
+  
+sys.excepthook = excepthook
+ 
 if __name__ == "__main__":
     
+    global basedir
 
     if len(sys.argv) < 1:
         print "Call as %s" % sys.argv[0]
         sys.exit(1)
 
-   
     if getattr(sys, 'frozen', None):
         basedir = sys._MEIPASS
     else:
         basedir = os.path.dirname(__file__)
-           
+
     if os.path.isfile(os.path.join(basedir, "preferences.json")):
         preferences.load(os.path.join(basedir, "preferences.json"))
     else:
         preferences.load(os.path.join(basedir, "defaults.json"))
-        
-        
-    setLogLevel(prefOrVal("main", "logLevel", INFO))
-    setFileLog(os.path.join(basedir, "yajsig.log"), prefOrVal("main", "fileLogLevel", DEBUG))
-    
+
+
+    setLogLevel(pref("main", "logLevel", INFO))
+    setFileLog(os.path.join(basedir, "yajsig.log"), pref("main", "fileLogLevel", DEBUG))
+
     global qapp    
     qapp = QApplication([])
 
 
-    try:
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        log(DEBUG, "Got %s:%s from command line." % (username, password))
+    else:
+        username = pref("jsit", "username", "")
+        password = pref("jsit", "password", "")        
+        log(DEBUG, "Got %s:%s from preferences." % (username, password))
 
-        if len(sys.argv) == 3:
-            username = sys.argv[1]
-            password = sys.argv[2]
-            log(DEBUG, "Got %s:%s from command line.\n" % (username, password))
-        else:
-            username = pref("jsit", "username")
-            password = pref("jsit", "password")        
-            log(DEBUG, "Got %s:%s from preferences.\n" % (username, password))
-           
-            if username == None or password == None:
-                log(DEBUG, "Need username and password, trigger input.\n")
-                raise jsit.APIError
-                
-        mgr = jsit_manager.Manager(username = username, password = password, torrentdir = pref("jsit", "torrentDirectory"))
-        
-    except jsit.APIError, e:
-        log(WARNING, "JSIT login failed!\n")
-        
-        username, ok = QInputDialog.getText(None, "JS.it Username", "Enter JS.it username:", QLineEdit.Normal, username)
-        
-        if not ok:
-            log(ERROR, "Username aborted!\n")
-            sys.exit(1)
-        
-        password, ok = QInputDialog.getText(None, "JS.it Password", "Enter JS.it password:", QLineEdit.Normal, password)
-        
-        if not ok:
-            log(ERROR, "Password aborted!\n")
-            sys.exit(1)
-        
-        mgr = jsit_manager.Manager(username = username, password = password)
-        
-        preferences.setValue("jsit", "username", username)
-        preferences.setValue("jsit", "password", password)
-   
-    # Hack...
+        if username == None or password == None:
+            log(DEBUG, "Need username and password, trigger input.")
+            raise jsit.APIError
+
+    while True:
+        try:
+
+            mgr = jsit_manager.Manager(username = username, password = password, torrentdir = pref("jsit", "torrentDirectory", "intorrents"))
+
+            break
+            
+        except Exception, e:
+            log(WARNING, "JSIT login failed (%s)!" % e)
+
+            username, ok = QInputDialog.getText(None, "JS.it Username", "Enter JS.it username:", QLineEdit.Normal, username)
+
+            if not ok:
+                log(ERROR, "Username aborted!")
+                sys.exit(1)
+
+            password, ok = QInputDialog.getText(None, "JS.it Password", "Enter JS.it password:", QLineEdit.Normal, password)
+
+            if not ok:
+                log(ERROR, "Password aborted!")
+                sys.exit(1)
+
+    log(DEBUG, "jsit_manager started...")
+
+    preferences.setValue("jsit", "username", username)
+    preferences.setValue("jsit", "password", password)
+
+    # Hack... Windows doesn't set cwd, so local paths go wrong
     if not mgr._torrentDirectory.startswith(os.path.sep) and not mgr._torrentDirectory[1] == ':':
         mgr._torrentDirectory = os.path.join(basedir, mgr._torrentDirectory)
-        
+    d = pref("downloads", "basedir", "downloads")
+    if not d.startswith(os.path.sep) and not d[1] == ':':
+        d = os.path.join(basedir, d)
+        preferences.setValue("downloads", "basedir", d)
+
     # For testing limit logging...
     ##addIgnoreModule("jsit")
     ##addIgnoreModule("jsit_manager")
     ##addOnlyModule("TorrentTable")
-    
+
+
+    # Put up a Splash Screen
+    splash_pix = QPixmap('yajsig_splash.png')
+    sc = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+    sc.show()
+    qapp.processEvents();
+    qapp.processEvents();
+
+
     win = JSITWindow(mgr)
 
-    if pref("GUI", "threadedUpdates"):
-        global updateThread, updateEvent, quitEvent
-        
-        updateThread = threading.Thread(target=updateThreadFunc, name="Updater", args = (win.model, win.ui.tableView))
-        updateEvent = threading.Event()
-        quitEvent = threading.Event()
-        
-        updateThread.start()
-
     QObject.connect(qapp, SIGNAL("lastWindowClosed()"), win, SLOT("quit()"))
- 
+
     win.show()
+    sc.finish(win)
+
     qapp.exec_()
