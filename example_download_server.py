35d43f97607497a70d563f2bdebd6b3093331545
TouchTone <touch_tone@mail.com>
2014-06-02 14:41:41 -0500

    Changed to use jsit_manager built-ins

--- example_download_server.py
+++ example_download_server.py
@@ -10,65 +10,19 @@ if len(sys.argv) < 3:
     print "Call as %s <username> <password>" % sys.argv[0]
     sys.exit(1)
 
-# Configuration vars
-
-downLabel = "GetMe"         # Only download torrents with this label, if None: get all
-gotLabel = "DLDone"         # Set torrents that have finished download to this label, ignore them later.
-downloadDir = "downloads"   # Directory to download everything to
-downloadMode = "Pieces"     # How to download torrents: Pieces: piece by piece, Finished: at the end using aria
-
-
-# Already downloaded/ing torrents
-downt = set()
-working = set()
-last = 0
+# Configuration is done in preferences
+preferences.load("preferences.json")
 
 with jsit_manager.Manager(sys.argv[1], sys.argv[2]) as jsm:
 
-    while True:
+    while not jsm.allFinished:
        
-        # Update torrents and running downloads
+        # Update torrents, running downloads and start new automatic downloads
         jsm.update()
-    
-        now = time.time()
         
-        # Check for new torrents to download?
-        if now > last + 20:
-            print "%s : Update: Manager has %d torrents." % (time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()), len(jsm))
-
-            # Check all torrents to see what we need
-            for t in jsm:
-
-                # Do we need this torrent?
-                if not t in downt and (not gotLabel or t.label != gotLabel) and (downLabel == None or t.label == downLabel):
-
-                    print "Adding %s for download..." % t.name
-
-                    t.downloadMode = downloadMode
-
-                    downt.add(t)
-                    working.add(t)
-
-            # Check if any of the working ones finished
-            if len(working):
-                print "Active:",
-                for t in set(working):
-                    print "%s at %.02f%% (%s, %s)" % (t.name, t.percentage, tools.isoize(t._torrent.data_rate_in, "b/s"), tools.isoize(t.downloadSpeed, "b/s")),
-
-                    if t.hasFinished:
-
-                        print "Finished!",
-                        if gotLabel:
-                            t.label = gotLabel
-
-                        working.remove(t)
-
-                print 
-            
-            last = now
-        
+        # Put a little sleep to avoid overhead
         time.sleep(2)
 
 print
-print "Done. Check out %s for the results." % downloadDir
+print "Done. Check out %s for the results." % prefs("downloads", "basedir", "downloads")
 
