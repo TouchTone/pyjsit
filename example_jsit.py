d4cc03aa91b7c27d3c490f1669ee732e398f6faf
TouchTone <touch_tone@mail.com>
2014-06-21 01:15:26 +0200

    Test more options, nicer printouts

--- example_jsit.py
+++ example_jsit.py
@@ -3,7 +3,7 @@
 ## Simple example for the JSIT module
 
 import time, sys
-from tools import testtorrents
+from tools import testtorrents, isoize_b, isoize_bps
 
 import jsit
 
@@ -24,13 +24,13 @@ with jsit.JSIT(sys.argv[1], sys.argv[2]) as js:
     print "Established connection to justseed.it. Let's see what you got going on..."
 
     # How many torrent do we have?
-    print "You're running %s torrents and you have %d bytes remaining." % (len(js), js.dataRemaining)
+    print "You're running %s torrents and you have %s remaining." % (len(js), isoize_b(js.dataRemaining))
 
     # How many are finished?
     print "Of those %d have already finished." % len([t for t in js if t.hasFinished])
 
     # How much data are we downloading?
-    print "And they all together have %d bytes of data." % reduce(lambda x,y: x + y.size, js, 0)
+    print "And they all together have %s of data." % isoize_b(reduce(lambda x,y: x + y.size, js, 0))
 
     # How many trackers do we use?
     trackers = set()
@@ -42,24 +42,34 @@ with jsit.JSIT(sys.argv[1], sys.argv[2]) as js:
 
     # Add a torrent
     print "Now let's do something: Adding new torrent..."
-    tor = js.addTorrentURL(testtorrents["Wallpapers"], maximum_ratio = 0.01)
+    tor = js.addTorrentURL(testtorrents["CreativeIdeas"], maximum_ratio = 0.01)
 
     print "Added torrent has hash %s." % tor.hash
 
     # Wait for it to finish
     while not tor.hasFinished:
-        print "%s is at %.02f%% %s doing %d b/s down and %d b/s up with %d peers (%d seeds)." % (tor.name, tor.percentage, tor.status, tor.data_rate_in, tor.data_rate_out, 
-                                                                                      len(tor.peers), len([p for p in tor.peers if p.percentage == 100]))
+        print "%s is at %.02f%% %s doing %s down and %s up with %d peers (%d seeds)." % (tor.name, tor.percentage, 
+                tor.status, isoize_bps(tor.data_rate_in), isoize_bps(tor.data_rate_out), 
+                len(tor.peers), len([p for p in tor.peers if p.percentage == 100]))
         time.sleep(10)
 
     print "Torrent %s finished!" % tor.name
 
+    # Peers
+    print "Currently with %d peers." % len(tor.peers)
+    
     # Files
     print "Torrent %s has %d files." % (tor.name, len(tor.files))
 
     f = tor.files[0]
     print "Here is the first one: %s (%d b), download from %s" % (f.path, f.size, f.url)
 
+    # Bitfield
+    print "Torrent bitfield is %s." % tor.bitfield
+    
+    # Torrent data
+    print "Raw torrent data starts with %s." % tor.torrent[0:10]
+    
     # Labels
     print "You've set these labels:", js.labels
 
