4fc513469c9f309cf8f58ceab2a1592f0da029cc
TouchTone <touch_tone@mail.com>
2014-06-23 00:56:44 +0200

    Add default dirs, cleanup

--- run_pyinstaller.bat
+++ run_pyinstaller.bat
@@ -2,6 +2,14 @@ set PATH=%PATH%;C:\Python27;C:\Python27\Scripts;C:\Scratch\upx391w;
 
 python update_prefs.py  --defaults
 
+rmdir /s /q dist
+
 pyinstaller --noconfirm yajsig.spec
 pyinstaller --noconfirm yajsis.spec
+
+python merge_dists.py dist/yajsi_all dist/yajsis dist/yajsig
+
+md dist\yajsi_all\downloads
+md dist\yajsi_all\completed
+
 
